07cc8535d22b157008b55d1787b66b9b6788c10a
boboshe <boboshesam@gmail.com>
2015-03-02 11:12:06 +0100

    Add Item & SimpleStack

---
+++ src/main/java/dcll/bter/mysimplestack/Item.java
@@ -0,0 +1,20 @@
+package dcll.bter.mysimplestack;
+
+/**
+ * Created by 21407957 on 02/03/2015.
+ */
+public class Item {
+    private Object obj;
+
+    public Item(Object obj) {
+        this.obj = obj;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+}
